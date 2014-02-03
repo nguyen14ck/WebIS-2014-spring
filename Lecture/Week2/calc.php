a4b273392fc24bcb6863b2e1e8cd5c97836e0b40
Timothy Middelkoop <MiddelkoopT@missouri.edu>
2014-02-03 12:25:18 -0600

    Week 3 work.

--- Lecture/Week2/calc.php
+++ Lecture/Week2/calc.php
@@ -1,11 +1,10 @@
-<?php
+<?php // WebIS Test App Copyright 2014 by Timothy Middelkoop License Apache 2.0
 
 class Calculator {
 	static function add($a,$b){
-		print "Calculator::add>";
+		// print "Calculator::add>";
 		return $a+$b;
 	}
 }
-
 
 ?>
